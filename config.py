ce23d6391849ac9a9ad3482554367fdd5aa9df7f
M T <miki761000@gmail.com>
2021-12-29 17:01:21 +0200

    initial commit

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
 from decouple import config
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-
-from db import db
-from resources.routes import routes
 
 
 class ProductionConfig:
@@ -26,22 +19,3 @@ class DevelopmentConfig:
         f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
         f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
     )
-
-
-class TestingConfig:
-    """Configurations for Testing, with a separate test database."""
-
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:ines123@localhost:5432/test_db"
-    DEBUG = True
-
-
-def create_app(config="config.DevelopmentConfig"):
-    app = Flask(__name__)
-    app.config.from_object(config)
-
-    api = Api(app)
-    migrate = Migrate(app, db)
-    CORS(app)
-    [api.add_resource(*route) for route in routes]
-    return app
